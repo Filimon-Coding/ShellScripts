041ffd56d14426ab9b14443ae93c252d53088689
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-01-05 02:19:19 +0100

    Initial commit

---
+++ script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# A simple script to print a message and display the current date.
+echo "Hello, this is the new script1!"
+echo "Today’s date is: $(date)"
+echo "Today is nice day"
+echo " Yeyy funker nå "
