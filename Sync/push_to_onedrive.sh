8e8b2ef642dae91f0a34205c67dc8804b3fc0cdf
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-01-05 03:38:42 +0100

    endelige ! kanskje

--- Sync/push_to_onedrive.sh
+++ Sync/push_to_onedrive.sh
@@ -6,7 +6,15 @@ LOCAL_DIR=~/school-files                # Your Linux directory for school files
 
 # Sync from Linux to OneDrive
 echo "Syncing from Linux folder to OneDrive..."
-rclone sync "$LOCAL_DIR" "$ONEDRIVE_REMOTE" --progress --log-file ~/rclone-push.log --log-level INFO
+rclone sync "$LOCAL_DIR" "$ONEDRIVE_REMOTE" \
+    --progress \
+    --max-age 1m \
+    --size-only \
+    --retries 3 \
+    --ignore-errors \
+    --log-file ~/rclone-push.log \
+    --log-level INFO
+
 if [ $? -eq 0 ]; then
     echo "Push completed successfully."
 else
