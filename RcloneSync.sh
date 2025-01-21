2b5549933ddd6b87351a54c4245aedbb31f59a06
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-01-21 17:16:37 +0100

    Commeted out toa avoid accedenly sync from pc and losing data from onedrive

--- RcloneSync.sh
+++ RcloneSync.sh
@@ -18,29 +18,33 @@ sync_from_onedrive() {
     fi
 }
 
-# Function to sync from local directory to OneDrive
-sync_to_onedrive() {
-    echo "Starting synchronization from local folder to OneDrive..."
-    rclone sync ~/local-folder onedrive-skole-sin:/ \
-        --progress \
-        --create-empty-src-dirs \
-        --include "**" \
-        --exclude "Thumbs.db" \
-        --exclude "desktop.ini" \
-        --exclude ".DS_Store"
+# Main menu Det er for forhindre at jeg med uhell rsync og taper data på onedrive.
 
-    if [ $? -eq 0 ]; then
-        echo "Synchronization from local folder to OneDrive completed successfully."
-    else
-        echo "Synchronization failed. Please check the logs."
-    fi
-}
+# Function to sync from local directory to OneDrive (commented out to prevent accidental execution)
+# sync_to_onedrive() {
+#     echo "Starting synchronization from local folder to OneDrive..."
+#     rclone sync ~/local-folder onedrive-skole-sin:/ \
+#         --progress \
+#         --create-empty-src-dirs \
+#         --include "**" \
+#         --exclude "Thumbs.db" \
+#         --exclude "desktop.ini" \
+#         --exclude ".DS_Store"
+
+#     if [ $? -eq 0 ]; then
+#         echo "Synchronization from local folder to OneDrive completed successfully."
+#     else
+#         echo "Synchronization failed. Please check the logs."
+#     fi
+# }
+
+
 
 # Main menu
 main_menu() {
     echo "Choose a synchronization operation:"
     echo "1) Sync from OneDrive to local folder"
-    echo "2) Sync from local folder to OneDrive"
+    echo "2) Sync from local folder to OneDrive -- Commented out already -- "
     echo -n "Enter your choice (1 or 2): "
     read choice
 
@@ -55,6 +59,7 @@ main_menu() {
             echo "Invalid choice. Exiting."
             ;;
     esac
+
 }
 
 # Run the main menu
--- RelaxMood.sh
+++ RelaxMood.sh
@@ -2,7 +2,7 @@
 
   open_firefox() {
       echo "Opening Firefox with fun pages..."
-      firefox "https://www.youtube.com/watch?v=bPjZmQAvk_8&list=PLP7dQLdOcWqkGqBSNOuXTETUmp6jBEg0-&index=1&autoplay=1" "https://www.twitch.tv/zenzen77" "https://www.reddit.com" "https://candidate.webcruiter.com/nb-no/Account/SpaLogin?ReturnUrl=%2F" "https://www.finn.no/job/fulltime/search.html?location=0.20001&q=utvikler+sommerjobb&stored-id=74761418" 2>/dev/null &
+      firefox "https://www.youtube.com/watch?v=bPjZmQAvk_8&list=PLP7dQLdOcWqkGqBSNOuXTETUmp6jBEg0-&index=1&autoplay=1" "https://www.twitch.tv/zenzen77" "https://www.reddit.com" "https://candidate.webcruiter.com/nb-no/Account/SpaLogin?ReturnUrl=%2F" "https://www.finn.no/job/fulltime/search.html?location=0.20001&q=utvikler+sommerjobb&stored-id=74761418" "https://www.instagram.com" 2>/dev/null &
   }
   open_discord() {
     echo "Opening discord "
