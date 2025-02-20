d917a4fd552a6749fdd2bff3d5bfeb3804b3cef9
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-02-20 20:30:46 +0100

    more lines

--- funnyJokesScript.sh
+++ funnyJokesScript.sh
@@ -9,6 +9,14 @@ surprised."
     "Why did the scarecrow win an award? Because he was outstanding in his
 field!"
     "How does a penguin build its house? Igloos it together."
+    "I told my computer I needed a break and now it won't stop sending me
+     Kit-Kat ads."
+     "Parallel lines have so much in common. It’s a shame they’ll never
+      meet."
+      "Did you hear about the mathematician who’s afraid of negative
+       numbers? He will stop at nothing to avoid them!"
+       "I used to play piano by ear, but now I use my hands."
+       "Why don't skeletons fight each other? They don't have the guts."
 )
 
 # Get the number of jokes
