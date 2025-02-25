0f76a1c0fd40a470732019f9bf74df7bc8a79f94
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-02-25 13:59:55 +0100

    ok skript

---
+++ skoleMappe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FOLDER_PATH="/media/neov/New Volume/OnedriveEverything/local-folder/Dokumenter/OsloMetITHele"
+
+if [ -d "$FOLDER_PATH" ]; then
+    xdg-open "$FOLDER_PATH" &  # Opens the folder in the default file manager
+    echo "Skole direktory mappe åpnes nå"
+else
+    echo "Feil: Mappen finnes ikke!"
+    exit 1
+fi
