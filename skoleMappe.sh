87c39f5007327382ae3a9167f9ce25111377efdf
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-03-22 12:22:24 +0100

    forbedret slik at den sjekekr mount først før forsok for åpne mappen

--- skoleMappe.sh
+++ skoleMappe.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-FOLDER_PATH="/media/neov/NewDisk/OnedriveEverything/local-folder/Dokumenter/OsloMetITHele"
+DISK="/dev/nvme1n1p2"
+Mount_Path="/media/neov/NewDisk/"
+FOLDER_PATH="$Mount_Path/OnedriveEverything/local-folder/Dokumenter/OsloMetITHele"
 
+# Hvis mappen ikke finnes, kan det være disken ikke er aktivt montert ennå
+if [ ! -d "$Mount_Path" ]; then
+    echo "Prøver å montere disken først..."
+    udisksctl mount -b "$DISK" 2>/dev/null
+    sleep 2
+fi
+# sjekker vi om skolemappen finnes
 if [ -d "$FOLDER_PATH" ]; then
     xdg-open "$FOLDER_PATH" &  # Opens the folder in the default file manager
     echo "Skole direktory mappe åpnes nå"
