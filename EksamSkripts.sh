b19ad6d6a95241044049010d8e106cb15a3da353
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-04-22 21:23:21 +0200

    Ubuntu med docker og apache2

--- EksamSkripts.sh
+++ EksamSkripts.sh
@@ -23,3 +23,16 @@ c=$(($b - $a))
 
 d=$(date -d@$c -u +%H:%M:%S)
 echo "$d"
+
+
+# Dockerfile for stanard ubuntu with apache2 in background husk .
+FROM ubuntu:latest
+
+RUN apt-get update
+RUN apt-get install nano -y
+RUN apt-get install apache2 -y
+
+COPY index.html /var/www/html
+
+CMD ["apachectl", "-D", "FOREGROUND"]
+
